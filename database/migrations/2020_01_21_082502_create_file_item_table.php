98e3ca13364ec1cf5759fd6550bc59f2373a5415
Mohamed Alkhiami <mhd.alkhiami@gmail.com>
2020-02-14 12:06:30 +0800

    edit schema

--- database/migrations/2020_01_21_082502_create_file_item_table.php
+++ database/migrations/2020_01_21_082502_create_file_item_table.php
@@ -4,11 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFileItemTable extends Migration {
+class CreateFileItemTable extends Migration
+{
 
 	public function up()
 	{
-		Schema::create('file_item', function(Blueprint $table) {
+		Schema::create('file_item', function (Blueprint $table) {
 			$table->increments('id');
 			$table->string('title', 250);
 			$table->integer('file_owner')->unsigned();
@@ -17,19 +18,19 @@ class CreateFileItemTable extends Migration {
 
 			$table->longText('file_url');
 
-			
-			$table->integer('signature_type')->unsigned();
-			$table->integer('sign_sequence')->unsigned();
-		
-			
-		
+
+			$table->integer('sign_id')->unsigned();
+			$table->integer('sign_sequence_id')->unsigned();
+
+
+
 			$table->string('total_signer');
 			$table->string('completed_signer');
 
 			$table->string('lock_status');
 			$table->string('lock_time');
 			$table->string('lock_code');
-			
+
 			$table->timestamp('created_date')->useCurrent();
 			$table->string('created_by');
 			$table->timestamp('modified_date')->useCurrent();
--- database/migrations/2020_01_21_082512_create_foreign_keys.php
+++ database/migrations/2020_01_21_082512_create_foreign_keys.php
@@ -5,100 +5,101 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 
-class CreateForeignKeys extends Migration {
+class CreateForeignKeys extends Migration
+{
 
 	public function up()
 	{
-		Schema::table('file_item', function(Blueprint $table) {
+		Schema::table('file_item', function (Blueprint $table) {
 			$table->foreign('file_owner')->references('user_id')->on('Users')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('file_item', function(Blueprint $table) {
+		Schema::table('file_item', function (Blueprint $table) {
 			$table->foreign('status_id')->references('status_id')->on('item_status')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('file_item', function(Blueprint $table) {
-			$table->foreign('signature_type')->references('sign_id')->on('signature_type')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+		Schema::table('file_item', function (Blueprint $table) {
+			$table->foreign('sign_id')->references('sign_id')->on('signature_type')
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('file_item', function(Blueprint $table) {
-			$table->foreign('sign_sequence')->references('sign_sequence_id')->on('sign_sequence_')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+		Schema::table('file_item', function (Blueprint $table) {
+			$table->foreign('sign_sequence_id')->references('sign_sequence_id')->on('sign_sequence_')
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('Singer_Info', function(Blueprint $table) {
+		Schema::table('Singer_Info', function (Blueprint $table) {
 			$table->foreign('Item_id')->references('id')->on('file_item')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('Singer_Info', function(Blueprint $table) {
+		Schema::table('Singer_Info', function (Blueprint $table) {
 			$table->foreign('user_id')->references('user_id')->on('Users')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('viewer_Info', function(Blueprint $table) {
+		Schema::table('viewer_Info', function (Blueprint $table) {
 			$table->foreign('Item_id')->references('id')->on('file_item')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('viewer_Info', function(Blueprint $table) {
+		Schema::table('viewer_Info', function (Blueprint $table) {
 			$table->foreign('user_id')->references('user_id')->on('Users')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('message_log', function(Blueprint $table) {
+		Schema::table('message_log', function (Blueprint $table) {
 			$table->foreign('Item_id')->references('id')->on('file_item')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('message_log', function(Blueprint $table) {
+		Schema::table('message_log', function (Blueprint $table) {
 			$table->foreign('user_id')->references('user_id')->on('Users')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
-		Schema::table('message_log', function(Blueprint $table) {
+		Schema::table('message_log', function (Blueprint $table) {
 			$table->foreign('action')->references('action_id')->on('file_action')
-						->onDelete('restrict')
-						->onUpdate('restrict');
+				->onDelete('restrict')
+				->onUpdate('restrict');
 		});
 	}
 
 	public function down()
 	{
-		Schema::table('file_item', function(Blueprint $table) {
+		Schema::table('file_item', function (Blueprint $table) {
 			$table->dropForeign('file_item_file_owner_foreign');
 		});
-		Schema::table('file_item', function(Blueprint $table) {
+		Schema::table('file_item', function (Blueprint $table) {
 			$table->dropForeign('file_item_status_id_foreign');
 		});
-		Schema::table('file_item', function(Blueprint $table) {
+		Schema::table('file_item', function (Blueprint $table) {
 			$table->dropForeign('file_item_signature_type_foreign');
 		});
-		Schema::table('file_item', function(Blueprint $table) {
+		Schema::table('file_item', function (Blueprint $table) {
 			$table->dropForeign('file_item_sign_sequence_foreign');
 		});
-		Schema::table('Singer_Info', function(Blueprint $table) {
+		Schema::table('Singer_Info', function (Blueprint $table) {
 			$table->dropForeign('Singer_Info_Item_id_foreign');
 		});
-		Schema::table('Singer_Info', function(Blueprint $table) {
+		Schema::table('Singer_Info', function (Blueprint $table) {
 			$table->dropForeign('Singer_Info_user_id_foreign');
 		});
-		Schema::table('viewer_Info', function(Blueprint $table) {
+		Schema::table('viewer_Info', function (Blueprint $table) {
 			$table->dropForeign('viewer_Info_Item_id_foreign');
 		});
-		Schema::table('viewer_Info', function(Blueprint $table) {
+		Schema::table('viewer_Info', function (Blueprint $table) {
 			$table->dropForeign('viewer_Info_user_id_foreign');
 		});
-		Schema::table('message_log', function(Blueprint $table) {
+		Schema::table('message_log', function (Blueprint $table) {
 			$table->dropForeign('message_log_Item_id_foreign');
 		});
-		Schema::table('message_log', function(Blueprint $table) {
+		Schema::table('message_log', function (Blueprint $table) {
 			$table->dropForeign('message_log_user_id_foreign');
 		});
-		Schema::table('message_log', function(Blueprint $table) {
+		Schema::table('message_log', function (Blueprint $table) {
 			$table->dropForeign('message_log_action_foreign');
 		});
 	}
