5054c6da7488a7d5164afbf9354c3b58b33175ef
Mohamed Alkhiami <mhd.alkhiami@gmail.com>
2020-02-16 16:14:09 +0800

    s

--- database/migrations/2020_01_21_082502_create_item_status_table.php
+++ database/migrations/2020_01_21_082502_create_item_status_table.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreateItemStatusTable extends Migration {
+class CreateItemStatusTable extends Migration
+{
 
 	public function up()
 	{
-		Schema::create('item_status', function(Blueprint $table) {
+		Schema::create('item_status', function (Blueprint $table) {
 			$table->increments('status_id');
 			$table->string('status_display');
 			$table->string('created_by');
@@ -20,6 +22,8 @@ class CreateItemStatusTable extends Migration {
 
 	public function down()
 	{
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 		Schema::drop('item_status');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	}
 }
