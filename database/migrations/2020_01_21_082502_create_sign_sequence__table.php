13329f42e0be4841a77bde5641e1572ca4d0b66c
Mohamed Alkhiami <mhd.alkhiami@gmail.com>
2020-02-16 16:13:02 +0800

    s

--- database/migrations/2020_01_21_082502_create_sign_sequence__table.php
+++ database/migrations/2020_01_21_082502_create_sign_sequence__table.php
@@ -2,16 +2,18 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSignSequenceTable extends Migration {
+class CreateSignSequenceTable extends Migration
+{
 
 	public function up()
 	{
-		Schema::create('sign_sequence_', function(Blueprint $table) {
+		Schema::create('sign_sequence_', function (Blueprint $table) {
 			$table->increments('sign_sequence_id');
 			$table->string('sign_sequence_display');
-			
+
 			$table->timestamp('created_date')->useCurrent();
 			$table->string('created_by');
 			$table->timestamp('modified_date')->useCurrent();
@@ -21,6 +23,8 @@ class CreateSignSequenceTable extends Migration {
 
 	public function down()
 	{
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 		Schema::drop('sign_sequence_');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	}
 }
