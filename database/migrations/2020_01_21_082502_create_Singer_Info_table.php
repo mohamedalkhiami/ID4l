f5600a8b801af297279c6ff00d7bc757bbc4731f
Mohamed Alkhiami <mhd.alkhiami@gmail.com>
2020-02-16 16:18:37 +0800

    s

--- database/migrations/2020_01_21_082502_create_Singer_Info_table.php
+++ database/migrations/2020_01_21_082502_create_Singer_Info_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateSingerInfoTable extends Migration
@@ -22,6 +23,8 @@ class CreateSingerInfoTable extends Migration
 
 	public function down()
 	{
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 		Schema::drop('singer_info');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	}
 }
