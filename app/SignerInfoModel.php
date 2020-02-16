fc3747ece9e652d5a60a0154643e17e8b29b4008
Mohamed Alkhiami <mhd.alkhiami@gmail.com>
2020-02-16 16:28:07 +0800

    s

--- app/SignerInfoModel.php
+++ app/SignerInfoModel.php
@@ -6,14 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class SignerInfoModel extends Model
 {
-  
-    protected $table = 'Singer_Info';
-    protected $fillable = ['Item_id','user_id','sequence'];
 
-    public $timestamps = false ;
+    protected $table = 'singer_info';
+    protected $fillable = ['Item_id', 'user_id', 'sequence'];
+
+    public $timestamps = false;
 
     protected $casts = [
         'divisions' => 'array',
     ];
-    
 }
