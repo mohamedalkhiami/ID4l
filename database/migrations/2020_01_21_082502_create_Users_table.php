8cc8d007cb93285df7e8889b8f47e0da12fe46a2
Mohamed Alkhiami <mhd.alkhiami@gmail.com>
2020-02-16 16:11:23 +0800

    s

--- database/migrations/2020_01_21_082502_create_Users_table.php
+++ database/migrations/2020_01_21_082502_create_Users_table.php
@@ -2,19 +2,21 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
+class CreateUsersTable extends Migration
+{
 
 	public function up()
 	{
-		Schema::create('Users', function(Blueprint $table) {
+		Schema::create('Users', function (Blueprint $table) {
 			$table->increments('user_id');
 			$table->string('email');
 			$table->string('first_name');
 			$table->string('last_name');
 			$table->string('Firebase_Token');
-			$table->timestamp('created_at')->useCurrent();	
+			$table->timestamp('created_at')->useCurrent();
 			$table->string('created_by');
 			$table->timestamp('modified_date')->useCurrent();
 			$table->string('Modified_by');
@@ -23,6 +25,8 @@ class CreateUsersTable extends Migration {
 
 	public function down()
 	{
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 		Schema::drop('Users');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	}
 }
--- database/migrations/2020_01_21_082502_create_signature_type_table.php
+++ database/migrations/2020_01_21_082502_create_signature_type_table.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSignatureTypeTable extends Migration {
+class CreateSignatureTypeTable extends Migration
+{
 
 	public function up()
 	{
-		Schema::create('signature_type', function(Blueprint $table) {
+		Schema::create('signature_type', function (Blueprint $table) {
 			$table->increments('sign_id');
 			$table->string('sign_type_displays');
 			$table->timestamp('created_date')->useCurrent();
@@ -20,6 +22,9 @@ class CreateSignatureTypeTable extends Migration {
 
 	public function down()
 	{
+
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
 		Schema::drop('signature_type');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
 	}
 }
